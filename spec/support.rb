d3db8a5d9889c9b8479bbd01783de204fbcafbfe
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-05-05 11:42:12 +0200

    add expand matcher

--- spec/support.rb
+++ spec/support.rb
@@ -54,6 +54,38 @@ RSpec::Matchers.define :match do |expected|
   end
 end
 
+RSpec::Matchers.define :expand do |values = {}|
+  match do |pattern|
+    @string  ||= nil
+    begin
+      expanded = pattern.expand(values)
+    rescue Exception
+      false
+    else
+      @string ? @string == expandend : expandend
+    end
+  end
+
+  chain :to do |string|
+    @string = string
+  end
+
+  failure_message_for_should do |pattern|
+    message = "expected %p to be expandable with %p" % [pattern, values]
+    begin
+      expanded = pattern.expand(values)
+      message << " and result in %p, but got %p" % [@string, expanded] if @string
+    rescue Exception => error
+      message << ", but raised %p" % error
+    end
+    message
+  end
+
+  failure_message_for_should_not do |pattern|
+    "expected %p not to be expandable with %p" % [pattern, values]
+  end
+end
+
 module Support
   module Pattern
     def pattern(pattern, options = nil, &block)
