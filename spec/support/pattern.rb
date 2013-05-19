cd61de22ea5fdc4374d3c1d3247056b6cf668710
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-05-19 22:18:25 +0200

    Tests should still run if one of the patterns raises an exception during instanziation

--- spec/support/pattern.rb
+++ spec/support/pattern.rb
@@ -5,13 +5,13 @@ module Support
 
       if options
         description << " with options %p" % [options]
-        instance = described_class.new(pattern, options)
+        instance = subject_for(pattern, options)
       else
-        instance = described_class.new(pattern)
+        instance = subject_for(pattern)
       end
 
       context description do
-        subject(:pattern) { instance }
+        subject(:pattern, &instance)
         its(:to_s) { should be == pattern }
         its(:inspect) { should be == "#<#{described_class}:#{pattern.inspect}>" }
         its(:names) { should be_an(Array) }
@@ -23,6 +23,13 @@ module Support
 
         instance_eval(&block)
       end
+    end
+
+    def subject_for(pattern, *args)
+      instance = described_class.new(pattern, *args)
+      proc { instance }
+    rescue Exception => error
+      proc { raise error }
     end
   end
 end
