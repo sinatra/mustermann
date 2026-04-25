3f31e82a522f6800806baa890388e7496b3779ea
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2026-04-25 11:35:37 +0200

    fix indentation

--- mustermann/lib/mustermann/regexp_based.rb
+++ mustermann/lib/mustermann/regexp_based.rb
@@ -16,9 +16,9 @@ module Mustermann
     # @see (see Mustermann::Pattern#initialize)
     def initialize(string, **options)
       super
-      regexp       = compile(**options)
-      @peek_regexp = /\A#{regexp}/
-      @regexp      = /\A#{regexp}\Z/
+      regexp           = compile(**options)
+      @peek_regexp     = /\A#{regexp}/
+      @regexp          = /\A#{regexp}\Z/
       @simple_captures = @regexp.named_captures.none? { |name, positions| positions.size > 1 || always_array?(name) }
     end
 
